b2552d300066c19f36ff1d79669066ab4cc97824
huanglun <492593760@qq.com>
2018-03-19 22:57:34 +0800

    第一个机器学习示例

--- python与机器学习实战/first_example.py
+++ python与机器学习实战/first_example.py
@@ -45,3 +45,34 @@ x = (x - x.mean()) / x.std()
 plt.figure()
 plt.scatter(x, y, c='g', s=6)
 plt.show()
+# 在（-2,4）这个区间上取100个点作为画图的基础
+x0 = np.linspace(-2, 4, 100)
+
+
+# 利用numpy的函数定义训练并返回多项式回归模型的函数
+# deg参数代表着模型参数中的n，亦即模型中多项式的次数
+# 返回的模型能够根据输入的x（默认是x0），返回相对应的预测的y
+def get_model(deg):
+    return lambda input_x=x0: np.polyval(np.polyfit(x, y, deg), input_x)
+
+
+# 根据参数n、输入的x、y返回相对应的损失
+def get_cost(deg, input_x, input_y):
+    return 0.5 * ((get_model(deg)(input_x) - input_y) ** 2).sum()
+
+
+# 定义测试参数集并根据它进行各种实验
+test_set = (1, 4, 10)
+for d in test_set:
+    # 输出相应的损失
+    print(get_cost(d, x, y))
+# 画出相应的图像
+plt.scatter(x, y, c='g', s=20)
+for d in test_set:
+    plt.plot(x0, get_model(d)(), label='degree = {}'.format(d))
+# 将横轴、纵轴的范围分别限制在（-2,4）、（10^5,8*10^5）
+plt.xlim(-2, 4)
+plt.ylim(1e5, 8e5)
+# 调用legend方法使曲线对应的label正确显示
+plt.legend()
+plt.show()
