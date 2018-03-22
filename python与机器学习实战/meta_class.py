d665874bee4260200a0592456ba769248e65b9a9
huanglun <492593760@qq.com>
2018-03-23 00:39:39 +0800

    元类

--- python与机器学习实战/meta_class.py
+++ python与机器学习实战/meta_class.py
@@ -65,3 +65,24 @@ class Hong(Zhang, Wang, Ming, metaclass=Mixture):
 
 
 test(Hong())  # 将会输出三行，依次是Eat:1 Sleep:1 和Study:2
+
+
+# metaclass是类的模板，所以必须从`type`类型派生：
+class ListMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['add'] = lambda self, value: self.append(value)
+        return type.__new__(cls, name, bases, attrs)
+
+
+# 有了ListMetaclass，我们在定义类的时候还要指示使用ListMetaclass来定制类，传入关键字参数metaclass
+class MyList(list, metaclass=ListMetaclass):
+    pass
+
+
+# 当我们传入关键字参数metaclass时，魔术就生效了，它指示Python解释器在创建MyList时，
+# 要通过ListMetaclass.__new__()来创建，在此，我们可以修改类的定义，
+# 比如，加上新的方法，然后，返回修改后的定义。
+# __new__()方法接收到的参数依次是：当前准备创建的类的对象；类的名字；类继承的父类集合；类的方法集合。
+L = MyList()
+L.add(1)
+print(L)
